5830615b9a9465d88d0e031f7f4e2a9876724642
Daine Mawer <hello@dainemawer.com>
2022-07-21 13:15:30 +0200

    added debug function

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import { getLCP, getFID, getCLS, getTTFB } from 'web-vitals'
 import { sendToAnalytics } from './scripts/send-to-analytics'
 
-export function initSubCollection() {
-  getCLS(sendToAnalytics)
-  getLCP(sendToAnalytics)
-  getFID(sendToAnalytics)
-  getTTFB(sendToAnalytics)
+export function initSubCollection(debug) {
+
+  if (debug) {
+    console.log('Debug mode enabled')
+    getCLS(console.log)
+    getLCP(console.log)
+    getFID(console.log)
+    getTTFB(console.log)
+  } else {
+    getCLS(sendToAnalytics)
+    getLCP(sendToAnalytics)
+    getFID(sendToAnalytics)
+    getTTFB(sendToAnalytics)
+  }
 }
