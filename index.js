feb3714bd9a01e98da7c6517d1173c473169b164
Daine Mawer <hello@dainemawer.com>
2022-07-21 13:08:50 +0200

    changed entry file function

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 import { getLCP, getFID, getCLS, getTTFB } from 'web-vitals'
 import { sendToAnalytics } from './scripts/send-to-analytics'
 
-const subcollection = subcollection || {};
-
-
-(function(vitals) {
-
-  vitals.sendMetrics = function() {
-    getCLS(sendToAnalytics)
-    getLCP(sendToAnalytics)
-    getFID(sendToAnalytics)
-    getTTFB(sendToAnalytics)
-  }
-
-})(subcollection)
+export function initSubCollection() {
+  getCLS(sendToAnalytics)
+  getLCP(sendToAnalytics)
+  getFID(sendToAnalytics)
+  getTTFB(sendToAnalytics)
+}
